f1e9266637e6303905c4378371ff6f0ee7ea2a3a
Nrik <nrikcss@hotmail.fr>
2024-11-12 14:34:11 +0100

    apirest

---
+++ todo-api/src/Config/Controllers/Database.php
@@ -0,0 +1,25 @@
+<?php
+namespace Config;
+
+class Database {
+// Paramètres de connexion à la base de données
+    private string $host = "localhost";
+    private string $dbname = "todo_list_api";
+    private string $username = "root";
+    private string $password = "";
+    private ?\PDO $connection = null;
+    public function getConnection(): \PDO {
+// Si la connexion n'existe pas encore
+        if ($this->connection === null) {
+            try {
+// Tentative de connexion à la base
+//TODO A vous de compléter la code
+            } catch(\PDOException $e) {
+// En cas d'erreur, on lance une exception
+                throw new \Exception("Connection error: " . $e->getMessage());
+            }
+        }
+// Retourne la connexion
+        return $this->connection;
+    }
+}
