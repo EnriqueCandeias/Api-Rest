5d620f3ed5667905b16749a8d22d9280e187e9c3
AgatheDups <158457323+AgatheDups@users.noreply.github.com>
2024-11-13 12:08:37 +0100

    Update Router.php

--- todo-api/src/Config/Router.php
+++ todo-api/src/Config/Router.php
@@ -17,18 +17,18 @@ class Router {
                 continue;
             }
 
-        // Transformer la route en expression régulière
-        $pattern = preg_replace('/\{(\w+)\}/', '(\d+)', $route['path']);
-        $pattern = "@^" . str_replace('/', '\/', $pattern) . "$@D";
-        if (preg_match($pattern, $uri, $matches)) {
-            // Enlever le premier élément (match complet)
-            array_shift($matches);
-            // Créer le contrôleur et appeler l'action
-            $controllerName = "Controllers\\" . $route['controller'];
-            $controller = new $controllerName();
-            call_user_func_array([$controller, $route['action']], $matches);
-            return;
-        }
+            // Transformer la route en expression régulière
+            $pattern = preg_replace('/\{(\w+)\}/', '(\d+)', $route['path']);
+            $pattern = "@^" . str_replace('/', '\/', $pattern) . "$@D";
+            if (preg_match($pattern, $uri, $matches)) {
+                // Enlever le premier élément (match complet)
+                array_shift($matches);
+                // Créer le contrôleur et appeler l'action
+                $controllerName = "Controllers\\" . $route['controller'];
+                $controller = new $controllerName();
+                call_user_func_array([$controller, $route['action']], $matches);
+                return;
+            }
         }
         // Route non trouvée
         http_response_code(404);
