6307ed3a798965ae98bbf478e4789e1e25d067c6
AgatheDups <158457323+AgatheDups@users.noreply.github.com>
2024-11-13 14:59:28 +0100

    add composer

---
+++ todo-api/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'webwizards/todo-api',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '49f6c0aaae5828e2de33219fd97605d4db361513',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'webwizards/todo-api' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '49f6c0aaae5828e2de33219fd97605d4db361513',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
